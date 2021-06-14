7e1d00d8175cfefd77ff36bf6f984e23652f19b6
clavy@123 <prince1.sen2000@gmail.com>
2021-06-14 18:37:12 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express=require('express');
+const app=express()
+const mongo =require('mongoose')
+require('dotenv').config()
+mongo.connect(process.env.DB, {useNewUrlParser: true,useUnifiedTopology:true}).then(()=>{
+    console.log("connection Suucessfull")
+})
+const WishSchema=new mongo.Schema({
+    wish:{
+      type:String,
+      required:true,
+      lowercase:true,
+    }
+})
+let WishModel=mongo.model('Wish',WishSchema)
+app.set('view engine','ejs')
+const parser=require("body-parser")
+middleware=[
+    parser.urlencoded({extended:true})
+]
+app.get('/',function(req,res){
+    res.render('C:\\Users\\HP\\wishlist\\views\\index')
+})
+const item=[];
+var i=0;
+app.post('/home',middleware,function(req,res){
+    item[i++]=req.body.num1
+    const wish=new WishModel({
+        wish:req.body.num1
+    })
+    wish.save().then(()=>{
+        console.log("saved successfull")
+    }).catch(e=>{
+        console.log(error)
+    })
+    res.render('C:\\Users\\HP\\wishlist\\views\\home',{
+       item
+    })
+})
+app.get('/home/:id',function(req,res){
+    item.splice(req.params.id,1)
+    res.render('C:\\Users\\HP\\wishlist\\views\\home',{
+        item
+     })
+})
+app.listen(process.env.PORT)
